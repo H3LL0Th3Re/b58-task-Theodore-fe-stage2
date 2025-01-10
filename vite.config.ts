a7b9212e1438e536f2530d6796e4d03e3829cb17
Theodore <79800511+H3LL0Th3Re@users.noreply.github.com>
2025-01-11 03:21:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    setupFiles: './src/setup.ts',
     coverage: {
       reporter: ['text', 'json', 'html'],
       exclude: ['node_modules/', 'src/test/**/*'],
