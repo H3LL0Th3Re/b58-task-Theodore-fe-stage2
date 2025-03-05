2f56b202ec8bf0242f1e26d8b342bf9f652284aa
Theodore <79800511+H3LL0Th3Re@users.noreply.github.com>
2025-03-05 12:38:19 +0700

    Update color-mode.tsx

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -1,10 +1,10 @@
 "use client"
-
+import React from "react"
 import type { IconButtonProps } from "@chakra-ui/react"
 import { ClientOnly, IconButton, Skeleton } from "@chakra-ui/react"
 import { ThemeProvider, useTheme } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
-import * as React from "react"
+// import * as React from "react"
 import { LuMoon, LuSun } from "react-icons/lu"
 
 export interface ColorModeProviderProps extends ThemeProviderProps {}
